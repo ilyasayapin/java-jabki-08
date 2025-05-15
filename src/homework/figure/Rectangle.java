3dbd730e7ac3124b69490d6d2956ec086f2121de
ilyasayapin <kite@nxt.ru>
2025-05-15 09:04:14 +0300

    Lesson 8. Homework

---
+++ src/homework/figure/Rectangle.java
@@ -0,0 +1,13 @@
+package homework.figure;
+
+public class Rectangle extends Shape {
+    int width;
+    int height;
+
+    public Rectangle (int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+
+}
