1b54db8622a88d40e7c4160027782283fd6e8014
ilyasayapin <kite@nxt.ru>
2025-05-20 17:16:16 +0300

    Lesson 8. Homework

--- src/homework/sound/Player.java
+++ src/homework/sound/Player.java
@@ -3,8 +3,8 @@ package homework.sound;
 public class Player {
 
     public static void playAllSounds(SoundEmitter[] devices) {
-        for (int i = 0; i < devices.length; i++) {
-            devices[i].makeSound();
+        for (SoundEmitter device : devices) {
+            device.makeSound();
         }
     }
 }
